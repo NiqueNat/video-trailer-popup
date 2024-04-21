cb6b69b1c7222ae5101aaaa686adfef10e168af8
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2024-04-21 15:25:58 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+    //calling the function//
+    const btn = document.querySelector('.btn');
+    const closeIcon = document.querySelector('.close-icon');
+    const trailerContainer = document.querySelector('.trailer-container');
+
+    const video = document.querySelector('video');
+    //adding event listener to remove active//
+    btn.addEventListener('click', () => {
+        trailerContainer.classList.remove('active');
+    });
+    //adding event listener to add active back//
+    closeIcon.addEventListener('click', () => {
+        trailerContainer.classList.add('active');
+        video.pause();
+        video.currentTime = 0;
+    });
